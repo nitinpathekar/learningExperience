3f3783dd778a7c7e11f7f131ba34775cafe98c89
Nitin Pathekar <nitinsardarpathekar@gmail.com>
2024-12-23 13:47:59 +0530

    learn functions

---
+++ WEB/10/script.js
@@ -0,0 +1,23 @@
+//function with parameter
+    function avg(num1,num2){
+        return (num1+num2)/2;
+    }
+// function call
+    console.log(avg(2,4));
+
+
+
+//function get fullname
+    function fullName(firstName,Surname){
+        return (firstName +" "+Surname);
+    }
+    console.log(fullName('Nitin','Pathekar'));
+    let func =function(){
+        console.log("letfunc functiom")
+    }
+    func();
+    const funct =()=>{
+        console.log("arrowfunction")
+    }
+
+    funct();
